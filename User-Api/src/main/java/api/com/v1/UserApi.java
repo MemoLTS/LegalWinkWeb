db11cd3501303b918caf309ec9e38ee774a776a1
MemoLTS <guil.toledo@duocuc.cl>
2026-09-10 14:22:45 -0300

    ""

---
+++ User-Api/src/main/java/api/com/v1/UserApi.java
@@ -0,0 +1,13 @@
+package api.com.v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApi.class, args);
+	}
+
+}
